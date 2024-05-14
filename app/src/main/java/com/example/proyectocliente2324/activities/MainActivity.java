b85b8a28364221bc22c14b72c2acf50cd684b7a7
alejandro jorge <alejorpas2@alu.edu.gva.es>
2024-05-14 15:50:45 +0200

    Commit

--- app/src/main/java/com/example/proyectocliente2324/activities/MainActivity.java
+++ app/src/main/java/com/example/proyectocliente2324/activities/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.proyectocliente2324.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.myweatherbase.R;
 import com.example.proyectocliente2324.activities.preferencias.IdiomSetUp;
+import com.example.proyectocliente2324.activities.preferencias.PreferenciasActivity;
 import com.example.proyectocliente2324.activities.preferencias.ThemeSetup;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         ThemeSetup.applyPreferenceTheme(getApplicationContext());
         IdiomSetUp.applyPreferenceIdiom(getApplicationContext());
 
-        crearCuenta= findViewById(R.id.bttnCrearCuenta);
+        crearCuenta = findViewById(R.id.bttnCrearCuenta);
         recordarContrasenya = findViewById(R.id.bttnRecordarContrasenya);
 
         recordarContrasenya.setOnClickListener(view -> {
@@ -32,6 +34,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.configuracion):
+                Intent intentPreferenciasActivity = new Intent(this, PreferenciasActivity.class);
+                startActivity(intentPreferenciasActivity);
+                return true;
+            case (R.id.exit):
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 
