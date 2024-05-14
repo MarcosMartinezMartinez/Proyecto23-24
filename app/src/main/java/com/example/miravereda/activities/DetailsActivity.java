9ccc336c6456446a79d682f9dd7fefd3922ba6b6
ajorgep <alejorpas2@alu.edu.gva.es>
2024-05-14 20:01:15 +0200

    Commit

--- app/src/main/java/com/example/miravereda/activities/DetailsActivity.java
+++ app/src/main/java/com/example/miravereda/activities/DetailsActivity.java
@@ -8,6 +8,8 @@ import es.ieslavereda.miravereda.R;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private Button
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
